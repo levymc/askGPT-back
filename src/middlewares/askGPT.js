1757281382c32a369eff98183d8662aa44c5ccb8
Levy <levymcruz@gmail.com>
2023-07-19 20:07:40 -0300

    att

--- src/middlewares/askGPT.js
+++ src/middlewares/askGPT.js
@@ -7,7 +7,7 @@ const texto = `
 NFS-e - NOTA FISCAL DE SERVIÇOS ELETRÔNICA - RPS 1660 Série 1, emitido em: 30/03/2022   NAMAH LTDA   R TOMAZ GONZAGA, 530 APT 1800   LOURDES - Belo Horizonte - MG - 30180143   TELEFONE: 3125149565   EMAIL: CONTATO@ACENDASUALUZ.COM.BR   CNPJ: 44.713.203/0001-09   INSCRIÇÃO MUNICIPAL: 13579130012   NÚMERO DA NOTA   202200000001659   COMPETÊNCIA   30/03/2022 00:00:00   CÓDIGO DE VERIFICAÇÃO   9c9091c4   DATA DE EMISSÃO   30/03/2022 21:42:44   DADOS DO TOMADOR   NOME / RAZÃO SOCIAL   Vânia Moreira   E-MAIL   vm.vania.moreira@gmail.com   TELEFONE   16997330060   ENDEREÇO   Não informado, 01   BAIRRO / DISTRITO   Não informado   CEP   00000000   MUNICÍPIO   Belo Horizonte   UF   MG   PAÍS   Brasil   CPF / CNPJ / OUTROS   115.171.148-96   INSCRIÇÃO MUNICIPAL INSCRIÇÃO ESTADUAL   DISCRIMINAÇÃO DOS SERVIÇOS   Workshop de Autoestima.   CÓDIGO DO SERVIÇO   8.02 / 080200188 - INSTRUÇÃO E TREINAMENTO, AVALIAÇÃO DE CONHECIMENTOS DE QUAISQUER NATUREZA   MUNICÍPIO ONDE O SERVIÇO FOI PRESTADO   3106200 / Belo Horizonte   NATUREZA DA OPERAÇÃO   Tributação no municipio   REGIME ESPECIAL DE TRIBUTAÇÃO: -   VALOR DOS SERVIÇOS: 350,00   (-) DESCONTOS: 0 (-) DEDUÇÕES: 0   (-) RETENÇÕES FEDERAIS: 0,00 (=) BASE DE CÁLCULO: 350,00   (-) ISS RETIDO NA FONTE: 0 (x) ALÍQUOTA: 3,00 %   VALOR LÍQUIDO: 350,00 (=) VALOR DO ISS: R$ 10,50   RETENÇÕES FEDERAIS   PIS: R$ 0,00 COFINS: R$ 0,00 IR: R$ 0,00 CSLL: R$ 0,00 INSS: R$ 0,00   OUTRAS INFORMAÇÕES   Valor aprox dos tributos: R$ 0,00 federal, R$ 0,00 estadual e R$ 0,00 municipal Fonte: IBPT 02C353.   powered by eNotas Gateway  Retorne o resultado em formato de planilha, com as informações em colunas. `
 
 export async function askGPT(req, res, next){
-
+    console.log(req.body)
     dotenv.config()
 
 
@@ -18,7 +18,7 @@ export async function askGPT(req, res, next){
 
     const history = []
     
-    console.log("aqui")
+    console.log(req.body)
     const user_input =  message + req.body.question //question("Digite seu input: ")
 
     const messages = [];
